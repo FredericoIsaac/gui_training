628b580916e75e2a84f26231ebba114100f9982b
Frederico Gago <fredyisaac@gmail.com>
2020-10-29 06:21:02 +0000

    classes and event binding

--- exe/entry_height.py
+++ exe/entry_height.py
@@ -7,10 +7,12 @@ root.geometry("400x400")
 
 
 def myClick():
+	global myLabel
 	hello = "Hello " + e.get()
 	myLabel = Label(root, text=hello)
 	e.delete(0, 'end')
 	myLabel.pack(pady=10)
+	#myButton["state"] = DISABLED
 
 e = Entry(root, width=50, font=('Helvetica', 30))
 e.pack(padx=10, pady=10)
@@ -18,6 +20,14 @@ e.pack(padx=10, pady=10)
 myButton = Button(root, text="Enter Your Name", command=myClick)
 myButton.pack(pady=10)
 
+# Overwrite the existing label (pack system)
+def myDelete():
+    	#myLabel.pack_forget()
+		myLabel.destroy()
+		myButton["state"] = NORMAL
+
+deleteButton = Button(root, text="Delete Text", command=myDelete)
+deleteButton.pack(pady=10)
 
 
 root.mainloop()
